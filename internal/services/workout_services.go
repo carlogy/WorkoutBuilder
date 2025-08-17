c43079a68775fe507ea5b3b694179e34ec173bf0
Carlo Yannuzzi <1carlogy.dev@gmail.com>
2025-08-17 11:46:22 -0400

    added workoutblock to handle super, tri, and giant set scenarios with workout exercises

--- internal/services/workout_services.go
+++ internal/services/workout_services.go
@@ -16,20 +16,26 @@ type Set struct {
 type WorkoutExercise struct {
 	ExerciseID id.UUID `json:"exerciseID"`
 	Sets       []Set   `json:"sets"`
+	Notes      string  `JSON:"notes,omitempty"`
+}
+
+type WorkoutBlock struct {
+	Exercises      []WorkoutExercise `json:"exercises"`
+	RestAfterBlock int               `json:"restAfterBlock"`
 }
 
 type Workout struct {
-	ID          id.UUID           `json:"id"`
-	Name        string            `json:"name"`
-	Description string            `json:"description"`
-	Exercises   []WorkoutExercise `json:"exercises"`
-	CreatedAt   time.Time         `json:"createdAt"`
-	ModifiedAt  time.Time         `json:"modifiedAt"`
+	ID             id.UUID        `json:"id"`
+	Name           string         `json:"name"`
+	Description    string         `json:"description"`
+	ExerciseBlocks []WorkoutBlock `json:"exerciseBlocks"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	ModifiedAt     time.Time      `json:"modifiedAt"`
 }
 
 func ConvertDBWorkoutToWorkout(d database.Workout) (Workout, error) {
 
-	exercises := make([]WorkoutExercise, 0)
+	exercises := make([]WorkoutBlock, 0)
 	if d.Exercises.Valid {
 		err := json.Unmarshal(d.Exercises.RawMessage, &exercises)
 		if err != nil {
@@ -38,12 +44,12 @@ func ConvertDBWorkoutToWorkout(d database.Workout) (Workout, error) {
 	}
 
 	return Workout{
-		ID:          d.ID,
-		Name:        d.Name,
-		Description: *NullStringToString(d.Description),
-		Exercises:   exercises,
-		CreatedAt:   *NullTimeToTime(d.CreatedAt),
-		ModifiedAt:  *NullTimeToTime(d.ModifiedAt),
+		ID:             d.ID,
+		Name:           d.Name,
+		Description:    *NullStringToString(d.Description),
+		ExerciseBlocks: exercises,
+		CreatedAt:      *NullTimeToTime(d.CreatedAt),
+		ModifiedAt:     *NullTimeToTime(d.ModifiedAt),
 	}, nil
 
 }
